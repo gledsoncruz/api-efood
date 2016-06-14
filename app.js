13f89349c97bcc5a3c49cfbc84606c2a5a55ef2e
Gledson Cruz <gledson.cruz@gmail.com>
2016-06-14 17:39:43 -0300

    add mongo uri

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/efood');
+require('./config/database.js')(process.env.MONGODB_URI);
 
 // mongodb://efood:efood123@ds049104.mongolab.com:49104/efood
 
